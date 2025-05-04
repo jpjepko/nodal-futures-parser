b5d5482b057e2802617fd5138570b705f0d1ecf2
John Paul Jepko <jpjepko@umich.edu>
2025-05-04 14:08:57 -0400

    remove split-dir carefuly after splitting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 from concurrent.futures import ProcessPoolExecutor
 import math
 import os
+import shutil
 
 import tabula
 import fitz # from pymupdf
@@ -19,10 +20,6 @@ def main():
     parser.add_argument("--split-dir", default="split")
     args = parser.parse_args()
 
-    # only run when split-dir does not exist
-    if os.path.isdir(args.split_dir):
-        raise RuntimeError(f"split-dir {args.split_dir} must not exist")
-
     print(f"splitting into {args.num_cores} files...")
     split_pdf(args.pdf_name, args.num_cores, args.split_dir)
 
@@ -31,6 +28,15 @@ def main():
     with ProcessPoolExecutor(max_workers=args.num_cores) as executor:
         res = list(executor.map(parse_pdf, split_paths))
     
+    # remove split dir
+    if os.path.isdir(args.split_dir):
+        old_files = os.listdir(args.split_dir)
+        for file in old_files:
+            if file.endswith(".pdf"):
+                fpath = os.path.join(args.split_dir, file)
+                os.remove(fpath)
+        os.rmdir(args.split_dir)
+
     print("merging...")
     merged = pd.concat(res, ignore_index=True)
     
@@ -61,8 +67,9 @@ def split_pdf(fname: str, num_files: int, out_dir: str):
     for i in range(num_files):
         split_doc = fitz.open()
         begin_page = i * pages_per_split
+        last_page = min(begin_page + pages_per_split - 1, len(doc) - 1) # inclusive
 
-        split_doc.insert_pdf(doc, from_page=begin_page, to_page=min(begin_page + pages_per_split - 1, len(doc) - 1))
+        split_doc.insert_pdf(doc, from_page=begin_page, to_page=last_page)
 
         split_fname = f"{str(i).zfill(padding)}.pdf"
         split_fpath = os.path.join(out_dir, split_fname)
@@ -80,7 +87,7 @@ def parse_pdf(fpath: str) -> pd.DataFrame:
         pd.DataFrame: Table parsed as a dataframe.
     """
     df_list = tabula.read_pdf(fpath, pages="all", multiple_tables=True, java_options=["-XX:ActiveProcessorCount=1"])
-    
+
     # debug
     # split_doc = fitz.open(fpath)
     # if len(df_list) != len(split_doc):
